62dcbd125a3f83843d5202ed3b6d5fc3ae12acd3
KassiePereira <32659615+KassiePereira@users.noreply.github.com>
2018-02-09 08:39:52 -0500

    Add files via upload

---
+++ Koding With Kassie/js/main.js
@@ -0,0 +1,13 @@
+$('.scroll-to').on('click', function(e) {
+
+    e.preventDefault();
+
+    var thisTarget = $(this).attr('href');
+
+    var targetOffset = $(thisTarget).offset().top;
+
+    $('html, body').animate({
+      scrollTop: targetOffset
+    }, 600);
+
+  });
